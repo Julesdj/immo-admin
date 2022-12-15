07f77875b67f90d17e9dafc185e65d2981c4a3cc
Kokutse Djoguenou <kokutse@Kokutses-MacBook-Pro.local>
2022-12-15 08:22:50 -0500

    Setup redux toolkit query configurations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { Provider } from "react-redux";
-import glogalReducer from "state";
 import "./index.css";
+import glogalReducer from "state";
+import api from "state/api";
 import App from "./App";
 
 const store = configureStore({
     reducer: {
         global: glogalReducer,
+        [api.reducerPath]: api.reducer,
     },
+    middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
